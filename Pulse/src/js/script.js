8efaa258cdcae6203b95ccb8b260296a86123b4f
Taiga <taiga507@gmail.com>
2023-04-24 22:41:44 +0300

    v1.57.0
    Create block with tabs

--- Pulse/src/js/script.js
+++ Pulse/src/js/script.js
@@ -1,17 +1,38 @@
-$('.owl-carousel').owlCarousel({
-    speed:1200,
-    loop:true,
-    margin:30,
-    nav:true,
-    dots:false,
-    autoHeight: true,
-    responsive: {
-        0:{
-            items:1,
-            nav:false
-        },
-        1024:{
-            items:1,
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        speed:1200,
+        loop:true,
+        margin:30,
+        nav:true,
+        dots:false,
+        autoHeight: true,
+        responsive: {
+            0:{
+                items:1,
+                nav:false
+            },
+            1024:{
+                items:1,
+            }
         }
-    }
-})
+    });
+    
+    $('ul.catalog__tabs').on('click', 'li:not(catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            });
+        });
+    };
+
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
+});
