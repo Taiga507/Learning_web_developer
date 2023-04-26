0681bec5b8d070481b804009d5b2c8c33156c46c
Taiga <taiga507@gmail.com>
2023-04-26 21:31:10 +0300

    v1.59.0
    Add modal windows

--- Pulse/src/js/script.js
+++ Pulse/src/js/script.js
@@ -17,11 +17,15 @@ $(document).ready(function(){
         }
     });
     
+    // Tabs
+
     $('ul.catalog__tabs').on('click', 'li:not(catalog__tab_active)', function() {
         $(this)
           .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
           .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
     });
+
+    // Hide in card
 
     function toggleSlide(item) {
         $(item).each(function(i) {
@@ -35,4 +39,21 @@ $(document).ready(function(){
 
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
+
+    // Modal
+
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+    });
+
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        })
+    });
 });
